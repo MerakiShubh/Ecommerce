0e9901346c13ed5f3b1f413dc352de8b82d84244
MerakiShubh <shubhamt.champ@gmail.com>
2024-05-20 20:28:38 +0530

    add first section of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality{" "}
+          <span className="text-blue-500">digital assets</span>.
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigitalHippo. Every asset on our platform is verified by
+          our team to ensure our highest quality standards.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-5">
+          <Link href="/products" className={buttonVariants()}>
+            Browose Trending
+          </Link>
+          <Button variant="ghost">Our quality promise &rarr;</Button>
+        </div>
+      </div>
+      {/* Todo: List the products*/}
+    </MaxWidthWrapper>
+  );
 }
