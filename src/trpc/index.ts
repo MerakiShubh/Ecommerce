7835a21d2ffdc7e8ddc00c6f0789c58e4f7f456a
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-04 02:23:57 +0530

    add type safety for client and server using trpc and react query

---
+++ src/trpc/index.ts
@@ -0,0 +1,6 @@
+import { publicProcedure, router } from "./trpc";
+export const appRouter = router({
+  anyApiRoute: publicProcedure.query(() => {}),
+});
+
+export type AppRouter = typeof appRouter;
