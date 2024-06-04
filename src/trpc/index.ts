490250e8f61e7b8e183ee0909997568770248e0a
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-04 11:06:59 +0530

    Remove all code to start fresh

--- src/trpc/index.ts
+++
@@ -1,8 +0,0 @@
-import { publicProcedure, router } from "./trpc";
-export const appRouter = router({
-  anyApiRoute: publicProcedure.query(() => {
-    return "hello";
-  }),
-});
-
-export type AppRouter = typeof appRouter;
